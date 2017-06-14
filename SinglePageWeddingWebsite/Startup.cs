d99db0988ea6ece7730c0b5af5dcc8bcddf6d474
Chris Hunter <chris-2k6@hotmail.co.uk>
2017-06-14 20:42:00 +0100

    Project add
    Wedding project has been added to the repository.

---
+++ SinglePageWeddingWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SinglePageWeddingWebsite.Startup))]
+namespace SinglePageWeddingWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
